2f27c88a2b78442a2737c5423e2b91da3f452ab7
beck <beck@li.com>
2022-07-21 19:06:01 +0800

    first commit

---
+++ day01_gittest/src/main/java/com/li/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.li.git;
+
+/**
+ * @author lyl
+ * @date 2022/7/21
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
