f1a5124521c1fc052561d097023a1b38bcf2ad65
beck <beck@li.com>
2022-07-21 19:17:01 +0800

    master commit

--- day01_gittest/src/main/java/com/li/git/GitTest.java
+++ day01_gittest/src/main/java/com/li/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello hot-test git4");
+
+        System.out.println("master test");
     }
 }
